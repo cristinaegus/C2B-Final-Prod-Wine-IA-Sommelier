34c7a88633e5d0e2ab9261fe8401b23c840da498
unknown <egusquizacristina@gmail.com>
2025-07-23 12:35:13 +0200

    subida a pythonanywhere

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 import sys
 import os
 
-# Ruta absoluta a tu proyecto
-path = '/home/Dell/PyhtonIA/Wine_IA_Production'
+# Ruta absoluta a tu proyecto en PythonAnywhere
+path = '/home/cegusquiza/Wine_IA_Production'
 if path not in sys.path:
     sys.path.append(path)
 
-# Activa el entorno virtual
-activate_this = '/home/Dell/PyhtonIA/Wine_IA_Production/venv/bin/activate_this.py'
+# Activa el entorno virtual (¡usa activate_this.py!)
+activate_this = '/home/cegusquiza/Wine_IA_Production/venv/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
